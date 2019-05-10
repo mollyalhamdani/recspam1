df1bf00758c056712e5d9269e5252b725184ae74
mollyalhamdani <48150434+mollyalhamdani@users.noreply.github.com>
2019-05-10 23:31:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("576499048786296853")
+setInterval(function() {
+channel.send(`Credit pls Credit pls Credit pls`);
+}, 30)
+})
+
+client.login(NTI0ODkzMzQ0NTg1NzQ0Mzg0.XNXYHQ.HFt_OiSOFRJg1YcE-DjABY56GHo);
